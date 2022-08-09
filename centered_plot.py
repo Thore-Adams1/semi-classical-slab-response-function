63ab8a2eaafb5a5b2bff016207abe9bd0d306fcb
hasan <hsheperia@gmail.com>
2022-08-09 20:56:26 +0100

    bounds now calculated for centered_plot.py

--- centered_plot.py
+++ centered_plot.py
@@ -14,7 +14,7 @@ import thesis_code as tc
 USAGE = """\
 centered_plot.py [-h] [thesis_code.py args]
 """
-REQUIRED_CONSTANTS = ["Kx", "L", "tau"]
+REQUIRED_CONSTANTS = ["L", "tau"]
 
 
 def main():
@@ -33,16 +33,37 @@ def main():
             raise RuntimeError("{} should be provided to this script.".format(c))
         if c in variable_params:
             raise RuntimeError("{} should be a constant parameter.".format(c))
-    
-    w_lower, w_upper = 0, 1
-    k_window = ["-v", f"w={w_lower}:{w_upper}:{args.omega_steps}"]
+    k_vals = []
+    if "Kx" in params:
+        k_vals = [params["Kx"]]
 
-    print("\n"+" ".join(
-        ["thesis_code.py"] + [
-        pipes.quote(s)
-        for s in given_args
-    ] + k_window
-    ))
+    if "Kx" in variable_params:
+        k_vals = variable_params["Kx"]
+    
+    to_remove_indices = [i for i, p in enumerate(given_args) if p.startswith("Kx")]
+    for i in reversed(to_remove_indices):
+        del given_args[i]
+    
+    print()    
+    for Kx in k_vals:
+        e_neg_kl = np.exp(-Kx * params["L"])
+        w_neg = np.sqrt((1-e_neg_kl) / 2)
+        w_pos = np.sqrt((1+e_neg_kl) / 2)
+        w_neg_bounds = [w_neg - (2 / params["tau"]), w_neg + (2 / params["tau"])]
+        w_pos_bounds = [w_pos - (2 / params["tau"]), w_pos + (2 / params["tau"])]
+
+        w_lower, w_upper = 0, 1
+        w_neg_k_window = ["-v", "w={}:{}:{}".format(*w_neg_bounds, args.omega_steps)]
+        w_pos_k_window = ["-v", "w={}:{}:{}".format(*w_pos_bounds, args.omega_steps)]
+
+        for k_window in [w_neg_k_window, w_pos_k_window]:
+            print(" ".join(
+                ["thesis_code.py"] + [
+                pipes.quote(s)
+                for s in given_args
+            ] + k_window
+            ))
+        print()
 
 
 
